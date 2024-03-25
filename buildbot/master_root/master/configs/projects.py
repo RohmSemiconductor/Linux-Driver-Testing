39a233e993afcac7e18b186c618c94d89fe18e51
KalleNiemi <kalleniemi@protonmail.com>
2024-03-25 16:11:15 +0200

    Builders get added with a get_builder function
    Adding builders work with get_builder function.
    This change makes it possible to further clean
    master.cfg.

--- buildbot/master_root/master/configs/projects.py
+++ buildbot/master_root/master/configs/projects.py
@@ -1,8 +1,13 @@
-###### PROJECTS
-# To trigger gitpoller on tag changes, use tag_change instead of ['branch_name'] in the 'branches'
+import sys 
+import os
+sys.path.append(os.path.abspath("."))
+
+from configs.factories import *
 
 tag_change = lambda ref: ref.startswith('refs/tags/')
 
+###### PROJECTS
+# To trigger gitpoller on tag changes, use tag_change instead of ['branch_name'] in the 'branches'
 projects = {}
 
 projects['test_linux']= {
@@ -13,7 +18,8 @@ projects['test_linux']= {
     'polling': 60,
     'scheduler_name': 'scheduler-test_linux',
     'builderNames': ["builder_test_linux"],
-#    'factory': factory_test_linux,
+    'workerNames': ["worker1"],
+    'factory': factory_test_linux,
 }
 projects['linux_next']={
     'name': 'linux_next',
