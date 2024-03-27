a1ec3629cc15f563c36714d8b6bc54746e606ff8
KalleNiemi <kalleniemi@protonmail.com>
2024-03-27 13:10:09 +0200

    factory_test_linux builds one test kernel module
    factory_test_linux builds one test kernel module.
    Cloning / updating sources for test kernel modules
    work now. Using alwaysUseLatest=True is neccessary
    when cloning another repository in a single
    factory; otherwise the git repository given in the
    scheduler is used.
    
    Changed the way extra .py files are imported:
    buildbot master upgrade recognized an error when
    the command checked master.cfg for errors. Error
    reported that configs and passwords are not
    recognized as modules.
    
    Added new directories to .gitignore. These
    directories reflect the changes in factory steps.

--- buildbot/master_root/master/configs/factories.py
+++ buildbot/master_root/master/configs/factories.py
@@ -4,7 +4,12 @@ from buildbot.plugins import util, steps
 
 # factory_test_linux
 factory_test_linux = util.BuildFactory()
-factory_test_linux.addStep(steps.Git(repourl='https://github.com/RohmSemiconductor/Linux-Driver-Testing.git', branch='test_linux', mode='incremental',name="Update source files from git"))
+factory_test_linux.addStep(steps.Git(repourl='https://github.com/RohmSemiconductor/Linux-Driver-Testing.git', branch='test_linux', mode='incremental',name="Update kernel source files from git"))
+factory_test_linux.addStep(steps.Git(repourl='https://github.com/RohmSemiconductor/Linux-Driver-Testing.git', branch='test-kernel-modules', alwaysUseLatest=True, mode='incremental', workdir="build/_test-kernel-modules", name="Update kernel module source files from git"))
+
+#factory_test_linux.addStep(steps.ShellCommand(command=["make", "-C", "../", "ARCH=arm", "CROSS_COMPILE=/home/user01/tools/beagle-dev-tools/bb-compiler/gcc-linaro-6.4.1-2017.11-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-", "M=_test-kernel-modules/"], workdir="build/_test-kernel-modules/overlay_merger", name="Build test kernel modules"))
+factory_test_linux.addStep(steps.ShellCommand(command=["make"], env={'KERNEL_DIR':'../../','CC':'/home/user01/tools/beagle-dev-tools/bb-compiler/gcc-linaro-6.4.1-2017.11-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-','PWD':'./'}, workdir="build/_test-kernel-modules/overlay_merger", name="Build test kernel modules"))
+
 factory_test_linux.addStep(steps.FileDownload(mastersrc="~/tools/kernel/.config",workerdest=".config",name="Copy kernel config to build directory"))
 factory_test_linux.addStep(steps.ShellCommand(command=["make", "-j8", "ARCH=arm", "CROSS_COMPILE=/home/user01/tools/beagle-dev-tools/bb-compiler/gcc-linaro-6.4.1-2017.11-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-", "LOADADDR=0x80008000", "olddefconfig"],name="Update kernel config if needed"))
 factory_test_linux.addStep(steps.ShellCommand(command=["make", "-j8", "ARCH=arm", "CROSS_COMPILE=/home/user01/tools/beagle-dev-tools/bb-compiler/gcc-linaro-6.4.1-2017.11-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-", "LOADADDR=0x80008000"],name="Build kernel binaries"))
@@ -29,3 +34,15 @@ factory_linux_next.addStep(steps.MultipleFileUpload(workersrcs=["arch/arm/boot/d
                                    masterdest="/var/lib/tftpboot/",
                                    mode=0o644,name="Upload compiled binaries to tftp directory"))
 factory_linux_next.addStep(steps.ShellCommand(command=["pytest", "--lg-env", "local.yaml", "test_shell.py"], workdir="../tests/first_test", name="Test script"))
+
+# factory_test_kernel_modules
+#factory_test_kernel_modules = util.BuildFactory()
+#factory_test_kernel_modules.addStep(steps.Git(repourl='https://github.com/RohmSemiconductor/Linux-Driver-Testing.git', branch='test-kernel-modules', mode='incremental',name="Update source files from git"))
+
+#factory_linux_next.addStep(steps.ShellCommand(command=["make", "-j8", "ARCH=arm", "CROSS_COMPILE=/home/user01/tools/beagle-dev-tools/bb-compiler/gcc-linaro-6.4.1-2017.11-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-", "LOADADDR=0x80008000", "INSTALL_MOD_PATH=/home/user01/nfs"],name="Install kernel modules"))
+#factory_linux_next.addStep(steps.ShellCommand(command=["dtc", "-@", "-I", "dts", "-O", "dtb", "-o", "/home/user01/Linux-Driver-Testing/buildbot/worker_root/worker1/linux-next/build/arch/arm/boot/dts/ti/omap/am335x-boneblack.dtb", "/home/user01/Linux-Driver-Testing/buildbot/worker_root/worker1/linux-next/build/arch/arm/boot/dts/ti/omap/.am335x-boneblack.dtb.dts.tmp"  ],name="Build device tree source binaries"))
+
+#factory_linux_next.addStep(steps.MultipleFileUpload(workersrcs=["arch/arm/boot/dts/ti/omap/am335x-boneblack.dtb", "arch/arm/boot/zImage"],
+#                                   masterdest="/var/lib/tftpboot/",
+#                                   mode=0o644,name="Upload compiled binaries to tftp directory"))
+#factory_linux_next.addStep(steps.ShellCommand(command=["pytest", "--lg-env", "local.yaml", "test_shell.py"], workdir="../tests/first_test", name="Test script"))
--- buildbot/master_root/master/configs/projects.py
+++ buildbot/master_root/master/configs/projects.py
@@ -1,8 +1,8 @@
 import sys 
 import os
-sys.path.append(os.path.abspath("."))
+sys.path.append(os.path.abspath("./configs"))
 
-from configs.factories import *
+from factories import *
 
 tag_change = lambda ref: ref.startswith('refs/tags/')
 
@@ -16,6 +16,7 @@ projects['test_linux']= {
     'branches': tag_change,
     'repo_git': 'https://github.com/RohmSemiconductor/Linux-Driver-Testing.git',
     'polling': 60,
+    'treeStableTimer': 30,
     'scheduler_name': 'scheduler-test_linux',
     'builderNames': ["builder_test_linux"],
     'builderNames': ["Test Linux"],
@@ -27,8 +28,19 @@ projects['linux-next']={
     'branches': tag_change,
     'repo_git': 'https://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git',
     'polling': 60,
+    'treeStableTimer': 600,
     'scheduler_name': 'scheduler-linux-next',
     'builderNames': ["linux-next"],
     'workerNames': ["worker1"],
     'factory': factory_linux_next,
 }
+#projects['test-kernel-modules']={
+#    'name': 'test-kernel-modules',
+#    'branches': ['test-kernel-modules'],
+#    'repo_git': 'https://github.com/RohmSemiconductor/Linux-Driver-Testing.git',
+#    'polling': 60,
+#    'scheduler_name': 'scheduler-test-kernel-modules',
+#    'builderNames': ["Test kernel modules"],
+#    'workerNames': ["worker1"],
+#    'factory': factory_test_kernel_modules,
+#}
