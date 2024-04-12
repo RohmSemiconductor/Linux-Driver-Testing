0c67edca9f9b784d7d49cdebfc983785206b1b8b
KalleNiemi <kalleniemi@protonmail.com>
2024-04-12 13:55:40 +0300

    Sequencing tests works. Added irc bot reporter.
    factories.py:
    
    Sequencing tests from buildbot works now. Additional
    change needed is to repeat rebooting of beaglebone,
    if shell login step fails.
    -
    Powercycling is done with a pytest script / test,
    the test_powercycle.py is not yet included in this
    commit.
    -
    
    master.cfg:
    
    IRC bot reporter added, and initially configured to
    join a channel and report if a build fails or succeeds.
    -
    Added configurator to clear older entries from logs than
    1 month.

--- buildbot/master_root/master/configs/factories.py
+++ buildbot/master_root/master/configs/factories.py
@@ -29,7 +29,7 @@ def build_kernel_arm32(project_name):
     projects[project_name]['factory'].addStep(steps.ShellCommand(command=["make", "-j8", "ARCH=arm", "CROSS_COMPILE="+dir_compiler_arm32+"arm-linux-gnueabihf-", "LOADADDR=0x80008000", "olddefconfig"],name="Update kernel config if needed"))
     projects[project_name]['factory'].addStep(steps.ShellCommand(command=["make", "-j8", "ARCH=arm", "CROSS_COMPILE="+dir_compiler_arm32+"arm-linux-gnueabihf-", "LOADADDR=0x80008000"],name="Build kernel binaries"))
     projects[project_name]['factory'].addStep(steps.ShellCommand(command=["make", "-j8", "ARCH=arm", "CROSS_COMPILE="+dir_compiler_arm32+"arm-linux-gnueabihf-", "LOADADDR=0x80008000", "INSTALL_MOD_PATH="+dir_nfs, "modules_install"],name="Install kernel modules"))
-    projects[project_name]['factory'].addStep(steps.ShellCommand(command=["dtc", "-@", "-I", "dts", "-O", "dtb", "-o", "/home/user01/Linux-Driver-Testing/buildbot/worker_root/worker1/Test_Linux/build/arch/arm/boot/dts/ti/omap/am335x-boneblack.dtb", "/home/user01/Linux-Driver-Testing/buildbot/worker_root/worker1/builder_test_linux/build/arch/arm/boot/dts/ti/omap/.am335x-boneblack.dtb.dts.tmp"  ],name="Build device tree source binaries")) 
+    projects[project_name]['factory'].addStep(steps.ShellCommand(command=["dtc", "-@", "-I", "dts", "-O", "dtb", "-o", dir_worker_root+projects[project_name]['workerNames'],'/',projects[project_name]['builderNames']+"/build/arch/arm/boot/dts/ti/omap/am335x-boneblack.dtb", dir_worker_root+projects[project_name]['workerNames'],'/',projects[project_name]['builderNames']+"/build/arch/arm/boot/dts/ti/omap/.am335x-boneblack.dtb.dts.tmp"],name="Build device tree source binaries")) 
 
 def build_test_kernel_modules(project_name):
     projects[project_name]['factory'].addStep(steps.Git(repourl='https://github.com/RohmSemiconductor/Linux-Driver-Testing.git', branch='test-kernel-modules', alwaysUseLatest=True, mode='full', workdir="build/_test-kernel-modules", name="Update kernel module source files from git"))
@@ -52,27 +52,32 @@ def upload_test_kernel_modules(project_name):
     projects[project_name]['factory'].addStep(steps.MultipleFileUpload(workersrcs=files,
     masterdest=dir_nfs,
     name="Upload test kernel modules to nfs directory"
-#+str(x)+"/"+str(len(kernel_modules['test']))
 ))
 
-def powercycle_ip_power(project_name, beagle_power_port, target):
+def download_test_boards(project_name):
+    projects[project_name]['factory'].addStep(steps.FileDownload(mastersrc="configs/kernel_modules.py",
+                            workerdest="../../tests/driver_tests/kernel_modules.py",
+                            name="Download kernel_modules.py"))
+#+str(x)+"/"+str(len(kernel_modules['test']))
 
-    projects[project_name]["factory"].addStep(steps.ShellSequence(
-    commands=[
-        util.ShellArg(command=["/bin/bash", "ip-power-control.sh", beagle_power_port, "0"]),
-        util.ShellArg(command=["/bin/bash", "ip-power-control.sh", beagle_power_port, "1"]),
-        ], workdir="../tests",flunkOnFailure=False ,flunkOnWarnings=False ,name=target+": Powercycling beagle bone"))
+
+#def powercycle_ip_power(project_name, beagle_power_port, target):
+#    projects[project_name]["factory"].addStep(steps.ShellSequence(
+#    commands=[
+#        util.ShellArg(command=["/bin/bash", "ip-power-control.sh", beagle_power_port, "0"]),
+#        util.ShellArg(command=["/bin/bash", "ip-power-control.sh", beagle_power_port, "1"]),
+#        ], workdir="../tests",flunkOnFailure=False ,flunkOnWarnings=False, decodeRC={0:0,1:0,2:0}, name=target+": Powercycling beagle bone"))
 
 def run_driver_tests(project_name):
     for test_board in test_boards:
-        for target in test_boards[test_board]['targets']:
-            powercycle_ip_power(project_name, test_boards[test_board]['power_port'], target)
-            projects[project_name]['factory'].addStep(steps.ShellCommand(command=["pytest", "--lg-env", test_boards[test_board]['name']+".yaml", "test_shell.py"], workdir="../tests/driver_tests", name=target+": Login to "+test_boards[test_board]['name']))
-            projects[project_name]['factory'].addStep(steps.ShellCommand(command=["pytest", "--lg-env", test_boards[test_board]['name']+".yaml", "test_init_overlay.py"], workdir="../tests/driver_tests", name=target+": Install overlay merger"))
-            projects[project_name]['factory'].addStep(steps.ShellCommand(command=["pytest","-ra", "--lg-env", test_boards[test_board]['name']+".yaml", "test_merge_dt_overlay.py","--product=bd71847"], workdir="../tests/driver_tests", name=target+": Merge device tree overlays"))
-            projects[project_name]['factory'].addStep(steps.ShellCommand(command=["pytest","-ra", "--lg-env", test_boards[test_board]['name']+".yaml", "test_insmod_tests.py","--product=bd71847"], workdir="../tests/driver_tests", name=target+": insmod test modules"))
-            projects[project_name]['factory'].addStep(steps.ShellCommand(command=["pytest","-ra", "--lg-env", test_boards[test_board]['name']+".yaml", "test_init_regulator_test.py","--product=bd71847"], workdir="../tests/driver_tests", name=target+": init_regulator_test.sh"))
-            projects[project_name]['factory'].addStep(steps.ShellCommand(command=["pytest","-ra", "--lg-env", test_boards[test_board]['name']+".yaml", "test_test_target.py","--product=bd71847"], workdir="../tests/driver_tests", name=target+": test_target.sh"))
+        for target in test_boards[test_board]['targets']: 
+            projects[project_name]['factory'].addStep(steps.ShellCommand(command=["pytest","-W","ignore::DeprecationWarning","-ra", "test_powercycle.py","--power_port="+test_boards[test_board]['power_port']], workdir="../tests/driver_tests", name=target+": Powercycle "+test_boards[test_board]['name']))
+            projects[project_name]['factory'].addStep(steps.ShellCommand(command=["pytest","-W","ignore::DeprecationWarning", "--lg-env", test_boards[test_board]['name']+".yaml", "test_shell.py"], workdir="../tests/driver_tests", name=target+": Login to "+test_boards[test_board]['name']))
+            projects[project_name]['factory'].addStep(steps.ShellCommand(command=["pytest","-W","ignore::DeprecationWarning", "--lg-env", test_boards[test_board]['name']+".yaml", "test_init_overlay.py"], workdir="../tests/driver_tests", name=target+": Install overlay merger"))
+            projects[project_name]['factory'].addStep(steps.ShellCommand(command=["pytest","-W","ignore::DeprecationWarning","-ra", "--lg-env", test_boards[test_board]['name']+".yaml", "test_merge_dt_overlay.py","--product="+target], workdir="../tests/driver_tests", name=target+": Merge device tree overlays"))
+            projects[project_name]['factory'].addStep(steps.ShellCommand(command=["pytest","-W","ignore::DeprecationWarning","-ra", "--lg-env", test_boards[test_board]['name']+".yaml", "test_insmod_tests.py","--product="+target], workdir="../tests/driver_tests", name=target+": insmod test modules"))
+            projects[project_name]['factory'].addStep(steps.ShellCommand(command=["pytest","-W","ignore::DeprecationWarning","-ra", "--lg-env", test_boards[test_board]['name']+".yaml", "test_init_regulator_test.py","--product="+target], workdir="../tests/driver_tests", name=target+": init_regulator_test.sh"))
+            projects[project_name]['factory'].addStep(steps.ShellCommand(command=["pytest","-W","ignore::DeprecationWarning","-ra", "--lg-env", test_boards[test_board]['name']+".yaml", "test_test_target.py","--product="+target], workdir="../tests/driver_tests", name=target+": test_target.sh"))
     
 # pytest --lg-env beagle1.yaml test_init_overlay.py
 # pytest -ra -v --lg-env beagle1.yaml test_merge_dt_overlay.py --product=bd71847
@@ -83,6 +88,7 @@ def linux_driver_test(project_name,beagle_ID,beagle_power_port):
     upload_kernel_binaries(project_name)
     build_test_kernel_modules(project_name)
     upload_test_kernel_modules(project_name)
+    download_test_boards(project_name)
 #    powercycle_ip_power(project_name,beagle_power_port)
     run_driver_tests(project_name)
 ### END OF HELPES ###
