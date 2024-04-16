ce10be48261a4c990706e8dee3c21183277d885d
KalleNiemi <kalleniemi@protonmail.com>
2024-04-16 12:02:00 +0300

    Changed README to make a change

--- gitpush.sh
+++ gitpush.sh
@@ -2,7 +2,7 @@
 git checkout test_linux
 echo 'x' >> README
 git add .
-git commit -m 'Linux 6.8-rc1'
+git commit -m 'Changed README to make a change'
 git push origin test_linux
 git tag $1
 git push origin $1
