9408d9f9abe55dad1202f44b118d0e914338b3d3
KalleNiemi <kalleniemi@protonmail.com>
2024-04-17 14:09:43 +0300

    startbuildbot.sh modified
    This script now uses buildbot start instead of
    restart. Change was made because as database gets
    bigger. Starting and stopping buildbot master gets
    slower.

--- startbuildbot.sh
+++ startbuildbot.sh
@@ -2,8 +2,8 @@
 
 #Start master
 source ~/Linux-Driver-Testing/buildbot/master_root/sandbox/bin/activate
-buildbot restart buildbot/master_root/master/
-
+buildbot start buildbot/master_root/master/
+sleep 1
 #Start worker
 source ~/Linux-Driver-Testing/buildbot/worker_root/sandbox/bin/activate
-buildbot-worker restart buildbot/worker_root/worker1
+buildbot-worker start buildbot/worker_root/worker1
