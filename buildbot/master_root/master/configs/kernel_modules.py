163ac189b888df2fcbadc406ff70254627ff982f
KalleNiemi <kalleniemi@protonmail.com>
2024-04-17 12:46:08 +0300

    Buildbot master changes, modified kernel_modules
    - master.cfg:
    Added change_commit_is_release/next(change)
    functions. These return True if regex string
    matches to commit message. These functions are
    used in get_release/next_scheduler(project_name)
    functions. These are to prevent unwanted changes
    from launching build/testing process.
    
    These are only used for linux kernel gits.
    
    Changed all get functions that had a space in
    builder name to have '_' instead.
    
    - factories.py:
    Step "Build device tree source binaries" now gets
    a string correctly to the ShellCommand.
    
    Steps powercycle beaglebone and logging in to
    beaglebone replaced with a single step that uses
    test_login.py
    
    - projects.py
    Using actual branches instead of tags, hoping
    this will be less resource intensive for
    gitpoller. Changed treeStableTimers and polling
    time as well.
    
    Changed builderNames with spaces to have _ instead
    This resulted in issues with a building steps using
    compiler.
    
    - kernel_modules.py
    kernel_modules['dt_overlays'] has all the device
    tree binary overlays.
    
    kernel_modules['test'] has only the test kernel
    modules, instead of including the .dtbo files.
    
    kernel_modules['merged_dt_overlay'] is used to
    assert in test_merge_dt_overlay.py
    
    kernel_modules['insmod_tests'] is used to assert
    in test_insmod_tests.py

--- buildbot/master_root/master/configs/kernel_modules.py
+++ buildbot/master_root/master/configs/kernel_modules.py
@@ -17,14 +17,15 @@ kernel_modules['dt_overlays']={
 'bd71828':['bd71828_test.dtbo'],#not yet installed
 'bd71837':['bd71837_test.dtbo','bbb_only_I2C_1.dtbo' ],
 'bd71847':['bd71847_test.dtbo'],
-'bd9576':['bd9576_test.dtbo'],
+'bd9576':['bd9576_test.dtbo']
 }
+
 kernel_modules['test']={
-'bd71815':['bd71815_test.dtbo','bd71815-test.ko','bd71815-gpio-test.ko','bd71815-clktest.ko'],
-'bd71828':['bd71828_test.dtbo','bd71828-test.ko','bd71828-gpio-test.ko','bd71828-clktest.ko'],#not yet installed
-'bd71837':['bd71837_test.dtbo','bd71837-test.ko'], #,'bbb_only_I2C_1.dtbo'
-'bd71847':['bd71847_test.dtbo','bd71847-test.ko','bd71847-test2.ko'],
-'bd9576':['bd9576_test.dtbo','bd9576-test.ko'],
+'bd71815':['bd71815-test.ko','bd71815-gpio-test.ko','bd71815-clktest.ko'],
+'bd71828':['bd71828-test.ko','bd71828-gpio-test.ko','bd71828-clktest.ko'],#not yet installed
+'bd71837':['bd71837-test.ko'], #,'bbb_only_I2C_1.dtbo'
+'bd71847':['bd71847-test.ko','bd71847-test2.ko'],
+'bd9576':['bd9576-test.ko']
 }
 
 #Used for assert: test_merge_dt_overlay.py, output of lsmod
@@ -33,7 +34,7 @@ kernel_modules['merged_dt_overlay']={
 'bd71828':['rohm_bd71828','gpio-bd71828','clk_bd718x7','bd71828_regulator','rtc_bd70528'], #lsmod OK(?) #not yet installed
 'bd71837':['bd718x7_regulator','rohm_regulator','clk_bd718x7','rohm_bd718x7'], #lsmod OK
 'bd71847':['bd718x7_regulator','rohm_regulator','clk_bd718x7','rohm_bd718x7'], #lsmod OK
-'bd9576':['bd9576_test.dtbo','bd9576-test.ko'],
+'bd9576':['bd9576_wdt','bd9576_regulator','rohm_bd9576'] #lsmod OK,
 }
 
 #Used for assert: test_insmod_tests.py, output of lsmod
@@ -41,7 +42,7 @@ kernel_modules['insmod_tests']={
 'bd71815':['bd71815_test','bd71815_gpio_test','bd71815_clktest'], #lsmod OK
 'bd71828':['bd71828_test.dtbo','bd71828-test.ko','bd71828-gpio-test.ko','bd71828-clktest.ko'],#not yet installed
 'bd71837':['bd71837_test'], #lsmod OK
-'bd71847':['bd71845_test','bd71847_test2'], #lsmod OK
+'bd71847':['bd71847_test','bd71847_test2'], #lsmod OK
 'bd9576':['bd9576_test'], #lsmod OK
 }
 
