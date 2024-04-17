dde5304446d580e002619ae4ea5be902d44f3614
KalleNiemi <kalleniemi@protonmail.com>
2024-04-17 12:25:10 +0300

    test_login.py handles powercycling and shell test
    Now test_login.py handles everything needed to test
    if beaglebone boots correctly. It still uses the
    shell script that uses telent.
    
    test_login is launched with command:
    pytest -ra test_login.py --power_port=1 --beagle=beagle1
    --power_port number and --beagle targets are given from
    buildbot.
    
    conftest.py has options / fixtures for parsing and
    forwarding the above launch options to the pytest
    files.

--- buildbot/worker_root/worker1/tests/driver_tests/conftest.py
+++ buildbot/worker_root/worker1/tests/driver_tests/conftest.py
@@ -10,13 +10,28 @@ def command(target):
     target.activate(shell)
     return shell
 
+#def pytest_addoption(parser):
+#    parser.addoption(
+#        "--product", action="store", default="type1", help="my option: type1 or type2"
+#    )
 
 def pytest_addoption(parser):
     parser.addoption(
+        "--power_port", action="store", default="type1", help="my option: type1 or type2"
+    )
+    parser.addoption(
         "--product", action="store", default="type1", help="my option: type1 or type2"
     )
-
+    parser.addoption(
+        "--beagle", action="store", default="type1", help="my option: type1 or type2"
+    )
 
 @pytest.fixture
 def product(request):
     return request.config.getoption("--product")
+@pytest.fixture
+def power_port(request):
+    return request.config.getoption("--power_port")
+@pytest.fixture
+def beagle(request):
+    return request.config.getoption("--beagle")
---
+++ buildbot/worker_root/worker1/tests/driver_tests/test_login.py
@@ -0,0 +1,26 @@
+
+from time import sleep
+import subprocess
+import sys
+import os
+sys.path.append(os.path.abspath("."))
+
+from helpers import *
+
+def test_login(power_port,beagle):
+    result = subprocess.run('/bin/bash .././ip-power-control.sh '+power_port+' 0' ,shell=True, capture_output=True, text=True)
+    result = subprocess.run('/bin/bash .././ip-power-control.sh '+power_port+' 1' ,shell=True, capture_output=True, text=True)
+    stdout=''
+    stdout = result.stdout.strip()
+    assert checkStr(stdout,'SetPowerByIdx')==0
+    
+    test_shell = subprocess.run('pytest --lg-env '+beagle+'.yaml test_shell.py',shell=True)
+    if test_shell.returncode != 0:
+        i = 0
+        while test_shell.returncode != 0 and i<5 :
+            i=i+1
+            result = subprocess.run('/bin/bash .././ip-power-control.sh '+power_port+' 0' ,shell=True, capture_output=True, text=True)
+            result = subprocess.run('/bin/bash .././ip-power-control.sh '+power_port+' 1' ,shell=True, capture_output=True, text=True)
+            test_shell = subprocess.run('pytest --lg-env beagle1.yaml test_shell.py',shell=True)
+
+    assert test_shell.returncode == 0
