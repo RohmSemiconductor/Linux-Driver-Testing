6bcab73083eb3340b0966838e663322c6acddc92
KalleNiemi <kalleniemi@protonmail.com>
2024-04-18 10:30:23 +0300

    Add linux-next specific files for 20240418

--- gitpush.sh
+++ gitpush.sh
@@ -2,7 +2,7 @@
 git checkout test_linux
 echo 'x' >> README
 git add .
-git commit -m 'Linux 6.8'
+git commit -m 'Add linux-next specific files for 20240418'
 git push origin test_linux
 git tag $1
 git push origin $1
