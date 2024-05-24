b92510f9a4e6c04b21bceb7b98d57cf61c79cb46
KalleNiemi <kalleniemi@protonmail.com>
2024-05-24 09:14:01 +0300

    Excluded wled from bd71815 out of range test
    Excluded wled from out of range test. This change keeps the test file
    cleaner.

--- buildbot/worker_root/worker1/tests/driver_tests/bd71815/test_003_out_of_range_voltages.py
+++ buildbot/worker_root/worker1/tests/driver_tests/bd71815/test_003_out_of_range_voltages.py
@@ -12,30 +12,27 @@ def test_out_of_range_voltages(command):
     test_failed =0
     failures=[]
     for regulator in bd71815.board.data['regulators'].keys():
-        print(regulator)
-        regulator_is_on=bd71815.regulator_is_on(regulator,command)
-
-        if ("volt_change_not_allowed_while_on" in bd71815.board.data['regulators'][regulator] and regulator_is_on == 1):
-            print("Cannot change regulator: "+regulator+" voltage - out of range tests skipped")
-
-        else:
-            min, max = bd71815.get_min_max_volt(regulator)
-            
-            bd71815.regulator_voltage_driver_set(regulator,min,command)
-            return_val_min = bd71815.regulator_voltage_get(regulator,command)
-
-            bd71815.regulator_voltage_driver_set(regulator,min-10000,command)
-            return_val_try_less = bd71815.regulator_voltage_get(regulator,command)
-            
-            assert return_val_min == return_val_try_less
-            
-            bd71815.regulator_voltage_driver_set(regulator,max,command)
-            return_val_max = bd71815.regulator_voltage_get(regulator,command)
-            bd71815.regulator_voltage_driver_set(regulator,max+10000,command)
-            return_val_try_more = bd71815.regulator_voltage_get(regulator,command)
-            
-            assert return_val_max == return_val_try_more
-
-
-#    test_fail = 0
-#    assert test_fail == 1
+        if regulator != 'wled':
+            print(regulator)
+            regulator_is_on=bd71815.regulator_is_on(regulator,command)
+    
+            if ("volt_change_not_allowed_while_on" in bd71815.board.data['regulators'][regulator] and regulator_is_on == 1):
+                print("Cannot change regulator: "+regulator+" voltage - out of range tests skipped")
+    
+            else:
+                min, max = bd71815.get_min_max_volt(regulator)
+                
+                bd71815.regulator_voltage_driver_set(regulator,min,command)
+                return_val_min = bd71815.regulator_voltage_get(regulator,command)
+    
+                bd71815.regulator_voltage_driver_set(regulator,min-10000,command)
+                return_val_try_less = bd71815.regulator_voltage_get(regulator,command)
+                
+                assert return_val_min == return_val_try_less
+                
+                bd71815.regulator_voltage_driver_set(regulator,max,command)
+                return_val_max = bd71815.regulator_voltage_get(regulator,command)
+                bd71815.regulator_voltage_driver_set(regulator,max+10000,command)
+                return_val_try_more = bd71815.regulator_voltage_get(regulator,command)
+                
+                assert return_val_max == return_val_try_more 
