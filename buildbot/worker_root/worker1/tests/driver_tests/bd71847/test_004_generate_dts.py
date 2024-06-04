271541b4ae7ab57ce24656aa57cfb8c7ecc16551
KalleNiemi <kalleniemi@protonmail.com>
2024-06-04 14:48:36 +0300

    Renamed test_dts.py to test_004_generate_dts.py
    This change was made to ensure correct sequence when BuildBot
    dynamically adds test steps in alphabetical order.

--- buildbot/worker_root/worker1/tests/driver_tests/bd71847/test_004_generate_dts.py
+++ buildbot/worker_root/worker1/tests/driver_tests/bd71847/test_004_generate_dts.py
@@ -3,15 +3,16 @@ import sys
 import os
 import fileinput
 from pathlib import Path
+from datetime import date
 sys.path.append(str(Path('./configs').absolute()))
 import bd71847_dts
 from pmic_conf import pmic
 bd71847 = pmic(bd71847_dts)
-print(sys.path)
 
-
-
+today = date.today()
+todaystr = today.strftime("%d-%m-%Y")
 def test_dts():
-    bd71847.generate_dts('protection_0','configs/bd71847_gen_template.dts','generated_test.dts')
+    bd71847.generate_dts('protection_0','configs/bd71847_gen_template.dts','configs/dts/bd71847/generated_test_'+todaystr+'.dts')
+    print(todaystr)
     test_fail = 0
     assert test_fail == 1
