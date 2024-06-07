c324752dd9f9d144c210df5b0c8da50d202dab90
KalleNiemi <kalleniemi@protonmail.com>
2024-06-07 10:59:54 +0300

    Added first test that test device tree properties
    test_004_default_dts.py:
    This tests ramprate properties that are set in the configs/bd71847_dts
    file. If other tests are not added, this file will get renamed.
    
    test_004_generate_dts.py renamed to test_005_generate_dts.py:
    This change is necessary for correct ordering of executing scripts.

--- buildbot/worker_root/worker1/tests/driver_tests/bd71847/test_005_generate_dts.py
+++ buildbot/worker_root/worker1/tests/driver_tests/bd71847/test_005_generate_dts.py
@@ -11,8 +11,6 @@ bd71847 = pmic(bd71847_dts)
 
 today = date.today()
 todaystr = today.strftime("%d-%m-%Y")
+conf_dts = 'ramprate2'
 def test_dts():
-    bd71847.generate_dts('protection_0','configs/bd71847_gen_template.dts','configs/dts/bd71847/generated_test_'+todaystr+'.dts')
-    print(todaystr)
-    test_fail = 0
-    assert test_fail == 1
+    bd71847.generate_dts(conf_dts, 'configs/bd71847_gen_template.dts','configs/dts/bd71847/generated_dts_'+conf_dts+'.dts')
