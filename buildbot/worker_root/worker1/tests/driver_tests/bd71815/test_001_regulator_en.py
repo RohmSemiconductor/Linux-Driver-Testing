8e15d04069a8665febb8e16df8226a605b071d0d
KalleNiemi <kalleniemi@protonmail.com>
2024-06-07 11:18:44 +0300

    Added sleep at the end of regulator enable test
    This sleep was added to the end of regulator enable test for the reason
    that i2c read may fail at the beginning or end of voltage run test. Most
    probable reason is that i2c voltage drops enough that the read fails.

--- buildbot/worker_root/worker1/tests/driver_tests/bd71815/test_001_regulator_en.py
+++ buildbot/worker_root/worker1/tests/driver_tests/bd71815/test_001_regulator_en.py
@@ -5,6 +5,7 @@ from pathlib import Path
 sys.path.append(str(Path('./configs').absolute()))
 import bd71815
 from pmic_conf import pmic
+from time import sleep
 bd71815 = pmic(bd71815)
 print(sys.path)
 
@@ -19,3 +20,4 @@ def test_regulator_en(command):
         if ((bd71815.check_regulator_enable_mode(regulator,command) == 1) and (bd71815.check_regulator_always_on_mode(regulator,command) == 0)):
             regulator_en_status = bd71815.regulator_disable(regulator,command)           
             assert regulator_en_status == 0
+    sleep(2)
