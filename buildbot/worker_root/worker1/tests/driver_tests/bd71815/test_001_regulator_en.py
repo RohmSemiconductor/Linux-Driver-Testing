528b931304ec4634abc4d43b0275b771733b1b56
KalleNiemi <kalleniemi@protonmail.com>
2024-06-12 15:11:17 +0300

    Added if statement to bd71815 regulator_en test
    This if statement was added to check that the regulator can be actually
    tested. 'dts_only' key was added to the regulators config file for this
    purpose.

--- buildbot/worker_root/worker1/tests/driver_tests/bd71815/test_001_regulator_en.py
+++ buildbot/worker_root/worker1/tests/driver_tests/bd71815/test_001_regulator_en.py
@@ -11,13 +11,16 @@ print(sys.path)
 
 def test_regulator_en(command):
     for regulator in bd71815.board.data['regulators'].keys():
-        if bd71815.check_regulator_enable_mode(regulator,command) == 1:
-            regulator_en_status = bd71815.regulator_enable(regulator,command)
-            assert regulator_en_status == bd71815.board.data['regulators'][regulator]['regulator_en_bitmask']
- 
-        regulator_en_status = bd71815.regulator_disable(regulator,command)
-        
-        if ((bd71815.check_regulator_enable_mode(regulator,command) == 1) and (bd71815.check_regulator_always_on_mode(regulator,command) == 0)):
-            regulator_en_status = bd71815.regulator_disable(regulator,command)           
-            assert regulator_en_status == 0
+
+        if not 'dts_only' in bd71815.board.data['regulators'][regulator].keys():
+
+            if bd71815.check_regulator_enable_mode(regulator,command) == 1:
+                regulator_en_status = bd71815.regulator_enable(regulator,command)
+                assert regulator_en_status == bd71815.board.data['regulators'][regulator]['regulator_en_bitmask']
+
+            regulator_en_status = bd71815.regulator_disable(regulator,command)
+
+            if ((bd71815.check_regulator_enable_mode(regulator,command) == 1) and (bd71815.check_regulator_always_on_mode(regulator,command) == 0)):
+                regulator_en_status = bd71815.regulator_disable(regulator,command)
+                assert regulator_en_status == 0
     sleep(2)
