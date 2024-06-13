f031b9bd6d790f7147c41c26c49bb656d6c5a7f7
KalleNiemi <kalleniemi@protonmail.com>
2024-06-13 12:38:14 +0300

    bd71847: fix data location for ramprate2 test
    bd71845_dts.py is used. These _dts.py files were not used anymore.
    
    Change test script to use bd71847.py.

--- buildbot/worker_root/worker1/tests/driver_tests/bd71847/dts/ramprate2/test_001_ramprate2_dts.py
+++ buildbot/worker_root/worker1/tests/driver_tests/bd71847/dts/ramprate2/test_001_ramprate2_dts.py
@@ -3,12 +3,17 @@ import sys
 import os
 from pathlib import Path
 sys.path.append(str(Path('./configs').absolute()))
-import bd71847_dts
+import bd71847
 from pmic_conf import pmic
-bd71847 = pmic(bd71847_dts)
+bd71847 = pmic(bd71847)
 print(sys.path)
 
-def test_001_ramprate2_dts(command):
-    test_dts = 'ramprate2'
-    property = 'ramprate'
-    bd71847.test_dts_properties(command, test_dts, property)
+def test_004_ramprate1(command):
+    for regulator in bd71847.board.data['regulators'].keys():
+        print(regulator)
+        if 'settings' in bd71847.board.data['regulators'][regulator].keys():
+            if 'ramprate' in bd71847.board.data['regulators'][regulator]['settings'].keys():
+                dt_setting = bd71847.read_dt_setting(regulator, 'ramprate', command)
+                print(dt_setting)
+                i2c_read = bd71847.i2c_to_ramprate_uv(regulator, command)
+                assert i2c_read == dt_setting
