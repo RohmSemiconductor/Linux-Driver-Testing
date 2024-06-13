95d83fc56601be299505a576dd160278113b1446
KalleNiemi <kalleniemi@protonmail.com>
2024-06-13 12:49:54 +0300

    bd9576: modified device tree test and added one
    test_004_errors_warnings renamed to test_001_ovd_uvd_disable. The device
    tree that this test uses disables protections and warnings.
    
    Added test_004_ovd_uvd1, this device tests ovd and uvd settings.
    ---
    These are now initial device tree tests, more may be added at later
    time.

--- buildbot/worker_root/worker1/tests/driver_tests/bd9576/test_004_ovd_uvd1.py
+++ buildbot/worker_root/worker1/tests/driver_tests/bd9576/test_004_ovd_uvd1.py
@@ -8,12 +8,12 @@ from pmic_conf import pmic
 bd9576 = pmic(bd9576)
 print(sys.path)
 
-def test_errors_warnings(command):
+def test_ovd_uvd(command):
     for regulator in bd9576.board.data['regulators'].keys():
         print(regulator)
-        for setting in bd9576.board.data['regulators'][regulator]['limit_settings'].keys():
+        for setting in bd9576.board.data['regulators'][regulator]['settings'].keys():
             print(setting)
-            dt_setting = bd9576.read_dt(regulator,'limit_settings', setting,command)
+            dt_setting = bd9576.read_dt_setting(regulator, setting, command)
             print("Device tree value: "+str(dt_setting))
             i2c_read = bd9576.i2c_to_lim_uv(regulator, setting, command)
             print("Read from i2c: "+str(i2c_read))
