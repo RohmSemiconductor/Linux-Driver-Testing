825d7b01e5b2d56b95cfc47936956927ed09a513
KalleNiemi <kalleniemi@protonmail.com>
2024-07-08 12:41:46 +0300

    BuildBot: Add bd71828 to test automation
    bd71828 is now connected to test automation setup.
    
    kernel_modules.py
    Remove comments describing that bd71828 is not yet installed.
    Fix modules that gets installed for regulator tests.
    
    test_boards.py:
    Add beagle3 where bd71828 is installed.

--- buildbot/master_root/master/configs/kernel_modules.py
+++ buildbot/master_root/master/configs/kernel_modules.py
@@ -15,7 +15,8 @@ kernel_modules['build']={
 'overlay_merger':['mva_overlay.ko'],
 'bd71815':['bd71815_test.dtbo','bd71815-test.ko','bd71815-gpio-test.ko','bd71815-clktest.ko'],
 'bd71828':['bd71828_test.dtbo','bd71828-test.ko','bd71828-gpio-test.ko','bd71828-clktest.ko'],
-'bd71837':['bd71837_test.dtbo','bd71837-test.ko','bbb_only_I2C_1.dtbo'],
+#'bd71837':['bd71837_test.dtbo','bd71837-test.ko','bbb_only_I2C_1.dtbo'],
+'bd71837':['bd71837_test.dtbo','bd71837-test.ko'],
 'bd71847':['bd71847_test.dtbo','bd71847-test.ko','bd71847-test2.ko'],
 'bd9576':['bd9576_test.dtbo','bd9576-test.ko'],
 'bd99954':['bd99954_test.dtbo'],
@@ -55,7 +56,7 @@ kernel_modules['dt_overlays']={
 
 kernel_modules['test']={
 'bd71815':['bd71815-test.ko','bd71815-gpio-test.ko','bd71815-clktest.ko'],
-'bd71828':['bd71828-test.ko','bd71828-gpio-test.ko','bd71828-clktest.ko'],#not yet installed
+'bd71828':['bd71828-test.ko','bd71828-gpio-test.ko','bd71828-clktest.ko'],
 'bd71837':['bd71837-test.ko'],
 'bd71847':['bd71847-test.ko','bd71847-test2.ko'],
 'bd9576':['bd9576-test.ko']
@@ -64,7 +65,8 @@ kernel_modules['test']={
 #Used for assert: test_merge_dt_overlay.py, output of lsmod
 kernel_modules['merged_dt_overlay']={
 'bd71815':['rohm_bd71828','gpio_bd71815','clk_bd718x7','bd71815_regulator','rtc_bd70528'],
-'bd71828':['rohm_bd71828','gpio-bd71828','clk_bd718x7','bd71828_regulator','rtc_bd70528'], #not yet installed
+#'bd71828':['rohm_bd71828','gpio_bd71828','clk_bd718x7','bd71828_regulator','rtc_bd70528'], #not yet installed
+'bd71828':['rohm_bd71828'],
 'bd71837':['bd718x7_regulator','rohm_regulator','clk_bd718x7','rohm_bd718x7'],
 'bd71847':['bd718x7_regulator','rohm_regulator','clk_bd718x7','rohm_bd718x7'],
 'bd9576':['bd9576_wdt','bd9576_regulator','rohm_bd9576']
@@ -73,7 +75,7 @@ kernel_modules['merged_dt_overlay']={
 #Used for assert: test_insmod_tests.py, output of lsmod
 kernel_modules['insmod_tests']={
 'bd71815':['bd71815_test','bd71815_gpio_test','bd71815_clktest'],
-'bd71828':['bd71828_test.dtbo','bd71828-test.ko','bd71828-gpio-test.ko','bd71828-clktest.ko'],#not yet installed
+'bd71828':['bd71828_test','bd71828_gpio_test','bd71828_clktest'],
 'bd71837':['bd71837_test'],
 'bd71847':['bd71847_test','bd71847_test2'],
 'bd9576':['bd9576_test'],
@@ -82,7 +84,7 @@ kernel_modules['insmod_tests']={
 #useless
 kernel_modules['init_regulator_test']={ #useless
 'bd71815':['ti_am335x_tscadc','industrilio','kfifo_buf','ti_am335x_adc'],
-'bd71828':['bd71828_test.dtbo','bd71828-test.ko','bd71828-gpio-test.ko','bd71828-clktest.ko'],#not yet installed
+'bd71828':['bd71828_test.dtbo','bd71828-test.ko','bd71828-gpio-test.ko','bd71828-clktest.ko'],
 'bd71837':['bd71837_test.dtbo','bd71837-test.ko'], #,'bbb_only_I2C_1.dtbo'
 'bd71847':['bd718x7_regulator','gpio_keys','rohm_regulator','clk_bd718x7','rohm_bd718x7'],
 'bd9576':['bd9576_test.dtbo','bd9576-test.ko'],
--- buildbot/master_root/master/configs/test_boards.py
+++ buildbot/master_root/master/configs/test_boards.py
@@ -2,7 +2,8 @@ test_boards ={}
 
 test_boards['beagle1']={
 'name':'beagle1',
-'targets':['bd71847','bd9576'],
+#'targets':['bd71847','bd9576'],
+'targets':['bd9576','bd71847'],
 'power_port':'1',
 'arch':'arm32',
 }
@@ -10,6 +11,14 @@ test_boards['beagle1']={
 test_boards['beagle2']={
 'name':'beagle2',
 'targets':['bd71815','bd71837'],
+#'targets':['bd71837'],
 'power_port':'2',
+'arch':'arm32',
+}
+
+test_boards['beagle3']={
+'name':'beagle3',
+'targets':['bd71828'],
+'power_port':'3',
 'arch':'arm32',
 }
