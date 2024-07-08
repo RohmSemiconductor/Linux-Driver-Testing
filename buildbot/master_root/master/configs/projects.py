859dec6cb963541128802832ffedf7f0f6db0b36
KalleNiemi <kalleniemi@protonmail.com>
2024-07-08 12:44:18 +0300

    BuildBot: projects: Describe dict with comments
    This commit is for anyone besides me trying to figure out how to use
    this test automation setup. This commented projects dictionary will be
    used in the documentation that is not yet done.
    
    Additional change:
    Increase tree stable timer to give a bit more slack so no builds will
    start too early.

--- buildbot/master_root/master/configs/projects.py
+++ buildbot/master_root/master/configs/projects.py
@@ -14,6 +14,18 @@ tag_change = lambda ref: ref.startswith('refs/tags/')
 # To trigger gitpoller on tag changes, use tag_change instead of ['branch_name'] in the 'branches'
 projects = {}
 
+projects['linux_mainline']={
+    'name': 'linux_mainline',                                                           # Used in factories.py to add steps to factory
+#    'branches': tag_change,                                                            # It is possible to poll for tag changes, but this proved problematic
+    'branches': ['master'],                                                             # Poll for a list of branches, even single branch needs to be in a list
+    'repo_git': 'https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git',   # This is the url for the git repository
+    'polling': 480,                                                                     # Polling interval in seconds
+    'treeStableTimer': 1100,                                                            # This is a timer which resets everytime a new change is made, helps during mergewindow so BB doesn't build every change
+    'scheduler_name': 'scheduler-linux_mainline',                                       # Scheduler used for the project, in master.cfg there is regex rules for schedulers: change_commit_is_release(change)
+    'builderNames': ["Linux_Mainline"],                                                 # Separate builder for each projects, these appear separately in the BuildBot Web view
+    'workerNames': ["worker1"],                                                         # List of workers which can build and run this project, worker1 is a local worker
+    'factory': factory_linux_mainline,                                                  # Factory used for this project
+}
 projects['test_linux']= {
     'name': 'test_linux',
     'branches': ['test_linux'],
@@ -34,23 +46,11 @@ projects['linux-next']={
     'branches': ['master'],
     'repo_git': 'https://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git',
     'polling': 480,
-    'treeStableTimer': 1000,
+    'treeStableTimer': 1100,
     'scheduler_name': 'scheduler-linux-next',
     'builderNames': ["linux-next"],
     'workerNames': ["worker1"],
     'factory': factory_linux_next,
-}
-projects['linux_mainline']={
-    'name': 'linux_mainline',
-#    'branches': tag_change,
-    'branches': ['master'],
-    'repo_git': 'https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git',
-    'polling': 480,
-    'treeStableTimer': 1000,
-    'scheduler_name': 'scheduler-linux_mainline',
-    'builderNames': ["Linux_Mainline"],
-    'workerNames': ["worker1"],
-    'factory': factory_linux_mainline,
 }
 projects['linux_stable']={
     'name': 'linux_stable',
@@ -59,7 +59,7 @@ projects['linux_stable']={
     'linux-6.8.y'],                                             #short time stable
     'repo_git': 'https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/',
     'polling': 480,
-    'treeStableTimer': 1000,
+    'treeStableTimer': 1100,
     'scheduler_name': 'scheduler-linux_stable',
     'builderNames': ["linux-stable"],
     'workerNames': ["worker1"],
