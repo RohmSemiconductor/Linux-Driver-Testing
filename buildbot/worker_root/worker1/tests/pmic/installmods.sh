5ee28f82370cf4042031e8edc9376ad8b294ed5e
KalleNiemi <kalleniemi@protonmail.com>
2024-07-16 15:21:08 +0300

    Dev tool: Tests: installmods.sh
    Modified installmods.sh to accept --type and --product that are used to
    write test report.

--- buildbot/worker_root/worker1/tests/pmic/installmods.sh
+++ buildbot/worker_root/worker1/tests/pmic/installmods.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /home/user01/Linux-Driver-Testing/buildbot/worker_root/sandbox/bin/activate
-pytest -ra test_login.py --beagle=beagle$2 --power_port=$2
+pytest -ra test_login.py --beagle=beagle$2 --power_port=$2 --type=PMIC --product=$1
 pytest --lg-env beagle$2.yaml test_init_overlay.py
 pytest -ra --lg-env beagle$2.yaml test_merge_dt_overlay.py --product=$1
 pytest -ra --lg-env beagle$2.yaml test_insmod_tests.py --product=$1
