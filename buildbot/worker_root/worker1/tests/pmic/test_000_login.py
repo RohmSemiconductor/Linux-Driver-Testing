bfbe53a8946ae5254cffd9341cbeda101be74010
KalleNiemi <kalleniemi@protonmail.com>
2024-07-18 10:50:22 +0300

    Tests: files: rename generic test scripts
    The directory tests/pmic has a lot of files and among them is the
    generic test steps. With files renamed to test_00#_* the sequence of
    these steps is clear.
    
    Modified BuildBot factories.py to match new names.
    
    _test_000_shell.py is a script called from test_000_login.py

--- buildbot/worker_root/worker1/tests/pmic/test_000_login.py
+++ buildbot/worker_root/worker1/tests/pmic/test_000_login.py
@@ -21,14 +21,14 @@ def test_login(power_port, beagle, type, product):
     check_result(result)
 
     result['stage'] = 'login'
-    test_shell = subprocess.run('pytest --lg-env '+beagle+'.yaml test_shell.py',shell=True)
+    test_shell = subprocess.run('pytest --lg-env '+beagle+'.yaml _test_000_shell.py',shell=True)
     if test_shell.returncode != 0:
         i = 0
         while test_shell.returncode != 0 and i<5 :
             i=i+1
             ip_power = subprocess.run('/bin/bash .././ip-power-control.sh '+power_port+' 0' ,shell=True, capture_output=True, text=True)
             ip_power = subprocess.run('/bin/bash .././ip-power-control.sh '+power_port+' 1' ,shell=True, capture_output=True, text=True)
-            test_shell = subprocess.run('pytest --lg-env '+beagle+'.yaml test_shell.py',shell=True)
+            test_shell = subprocess.run('pytest --lg-env '+beagle+'.yaml _test_000_shell.py',shell=True)
 
     result['expect'] = [power_port, beagle, 0]
     result['return'] = [power_port, beagle, test_shell.returncode]
