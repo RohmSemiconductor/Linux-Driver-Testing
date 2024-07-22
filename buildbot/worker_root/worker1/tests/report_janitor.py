41ebcbb06d263fc1a8bc1616f6ed23944ff5ebba
KalleNiemi <kalleniemi@protonmail.com>
2024-07-22 08:29:46 +0300

    Tests: test results: report_janitor: 'finalize'
    Add functionality to report_janitor to finalize test results. This will
    copy the temp_results.txt to directory corresponding to target name.
    
    ----
    For now this is not so throughoutly thought through. Git step is still
    missing too to upload results to github.

--- buildbot/worker_root/worker1/tests/report_janitor.py
+++ buildbot/worker_root/worker1/tests/report_janitor.py
@@ -1,8 +1,24 @@
 import sys
-
+import subprocess
+from datetime import datetime, timezone
+from time import sleep
 sys.path.append('.')
 from test_util import initialize_report
+
 if sys.argv[1] == 'initialize_report':
     bb_project = sys.argv[2]
     linux_ver = sys.argv[3]
     initialize_report(bb_project, linux_ver)
+
+elif sys.argv[1] == 'finalize':
+    bb_project = sys.argv[2]
+    linux_ver = sys.argv[3]
+    date = datetime.now()
+    date = date.strftime('%Y_%m_%d_%H%M%S')
+    if len(sys.argv)>3:
+        target = sys.argv[4]
+        stdout = subprocess.run('rm -rf ./results/'+target, shell=True)
+        stdout = subprocess.run('mkdir ./results/'+target, shell=True)
+        stdout = subprocess.run('cp ./results/temp_results.txt ./results/'+target, shell=True)
+        stdout = subprocess.run('mv ./results/'+target+'/temp_results.txt ./results/'+target+'/'+date+'_'+bb_project+'_'+target+'.txt', shell=True)
+        print(date)
