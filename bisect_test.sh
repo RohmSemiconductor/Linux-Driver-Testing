7424090fa4e5988e2b846ed24c39564a9ae76e13
KalleNiemi <kalleniemi@protonmail.com>
2024-08-02 08:43:51 +0300

    clean commit

---
+++ bisect_test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+git checkout test_linux
+echo 'x' >> README
+git add .
+git commit -m "clean commit"
+git push origin test_linux
+
+cp ../bugged_bd718x7-regulator.c drivers/regulator/bd718x7-regulator.c
+git add .
+git commit -m "bugged bd718x8-regulator.c"
+git push origin test_linux
+
+echo 'x' >> README
+git add .
+git commit -m "bad commit"
+git push origin test_linux
+
+cp ../bd718x7-regulator.c drivers/regulators/
