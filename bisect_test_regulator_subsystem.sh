7b4defe9c7c1420348b86168d64d501abca1108a
KalleNiemi <kaleposti@gmail.com>
2024-08-06 13:51:22 +0300

    clean commit

---
+++ bisect_test_regulator_subsystem.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo 'Push 1'
+echo '-------'
+git checkout test_linux
+echo 'x' >> README
+git add .
+git commit -m "clean commit"
+git push origin test_linux
+echo '-------'
+
+echo 'Push 2'
+echo '-------'
+cp ../bugged2_driver.h include/linux/regulator/driver.h
+git add .
+git commit -m "bugged driver.h"
+git push origin test_linux
+echo '-------'
+
+echo 'Push 3'
+echo '-------'
+echo 'x' >> README
+git add .
+git commit -m "bad commit"
+git push origin test_linux
+echo '-------'
+
+cp ../driver.h include/linux/regulator/driver.h
