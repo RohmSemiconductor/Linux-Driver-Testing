e43cb4be837cfb7955259ca93a501235bd7431bf
KalleNiemi <kaleposti@gmail.com>
2024-08-16 12:18:54 +0300

    Good commit

--- 3gitpush.sh
+++ 3gitpush.sh
@@ -10,8 +10,19 @@ echo '-------'
 
 echo 'Push 2'
 echo '-------'
+echo 'x' >> README
 git add .
 git commit -m 'Pull the plug!'
 git push origin test_linux
+echo '-------'
+
+echo 'Push 3'
+echo '-------'
+echo 'x' >> README
+git add .
+git commit -m 'Bad commit #2!'
+git push origin test_linux
+echo '-------'
+
 git tag $1
 git push origin $1
