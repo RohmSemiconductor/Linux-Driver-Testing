95e1550157840bdc10deb32a0cc44d7466b0410e
KalleNiemi <kaleposti@gmail.com>
2024-08-16 14:22:26 +0300

    Test Linux

---
+++ 3gitpush.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo 'Push 1'
+echo '-------'
+git checkout test_linux
+echo 'x' >> README
+git add .
+git commit -m 'Good commit'
+git push origin test_linux
+echo '-------'
+
+echo 'Push 2'
+echo '-------'
+echo 'x' >> README
+git add .
+git commit -m 'Pull the plug!'
+git push origin test_linux
+echo '-------'
+
+echo 'Push 3'
+echo '-------'
+echo 'x' >> README
+git add .
+git commit -m 'Bad commit #2!'
+git push origin test_linux
+echo '-------'
+
+git tag $1
+git push origin $1
---
+++ bisect_test_regulator_subsystem.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo 'Push 1'
+echo '-------'
+git checkout test_linux
+echo 'x' >> README
+git add .
+git commit -m "clean commit"
+git push origin test_linux
+echo '-------'
+
+echo 'Push 2'
+echo '-------'
+cp ../bugged_driver.h include/linux/regulator/driver.h
+git add .
+git commit -m "bugged driver.h"
+git push origin test_linux
+echo '-------'
+
+echo 'Push 3'
+echo '-------'
+echo 'x' >> README
+git add .
+git commit -m "bad commit"
+git push origin test_linux
+echo '-------'
+git tag $1
+git push origin $1
+
+cp ../driver.h include/linux/regulator/driver.h
