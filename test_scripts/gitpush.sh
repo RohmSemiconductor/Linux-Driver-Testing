8e17c6bf510e49b9a2fe543fa20a9f776f867561
KalleNiemi <kaleposti@gmail.com>
2024-09-20 08:21:02 +0300

    Linux 6.11-t6
    Fix bd718x7 to non-bugged version.

--- test_scripts/gitpush.sh
+++ test_scripts/gitpush.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-git checkout test_linux
-echo 'x' >> ../README
-git add ../.
+cd ..
+#git checkout test_linux
+echo 'x' >> README
+git add .
 git commit
 git push origin test_linux
 git tag $1
