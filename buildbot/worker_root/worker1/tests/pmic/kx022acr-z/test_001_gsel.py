a61c6212ea514bda5631a3bc6de9d03a582b6b47
Kalle Niemi <kaleposti@gmail.com>
2024-10-08 15:03:54 +0300

    Tests: Sensor: KX022ACR-Z: GSEL test: remove print
    Remove useless print from the test that was used to initially test the
    sensor class and the functions' return values.

--- buildbot/worker_root/worker1/tests/pmic/kx022acr-z/test_001_gsel.py
+++ buildbot/worker_root/worker1/tests/pmic/kx022acr-z/test_001_gsel.py
@@ -10,5 +10,4 @@ kx022acr_z = sensor(kx022acr_z)
 def test_gsel(command):
     for value in kx022acr_z.board.data['settings']['gsel']['list_values']:
         result = kx022acr_z.test_gsel(value, command)
-        print(result)
     check_result(result)
