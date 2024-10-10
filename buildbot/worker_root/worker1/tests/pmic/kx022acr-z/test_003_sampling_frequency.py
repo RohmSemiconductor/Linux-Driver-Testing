e78d9395f179ef20ab433c08b31cb893201858f6
Kalle Niemi <kaleposti@gmail.com>
2024-10-10 09:36:21 +0300

    Tests: Sensor: KX022ACR-Z: Update sample rate test
    Functions to do sampling rate tests have been changed in manner that it
    can be defined in the test script how many samples are taken, and
    tolerance is absolute percent instead of decimal value.
    
    Change 'tolerance'  decimal value to percent value.
    
    Use a function to increase the timestamp reading count value related to
    the tested sampling frequency. This function uses math.ceil() to round
    up. A normal round could be used, but this is the one that atleast never
    rounds to 0.
    
    Add function call to set watermark before reading timestamps.
    
    Note that a dummy read must be done before setting watermark, as
    described in the test scripts comment. Eventually this is not always
    necessary to do the dummy_read, if there is tests that do reads before
    setting the watermark. In this development phase it is time saving.
    
    ----
    Maybe it is good idea to consider doing a dummy read in sanity check,
    then test setting watermark and set it back to where it was.

--- buildbot/worker_root/worker1/tests/pmic/kx022acr-z/test_003_sampling_frequency.py
+++ buildbot/worker_root/worker1/tests/pmic/kx022acr-z/test_003_sampling_frequency.py
@@ -6,11 +6,17 @@ import kx022acr_z
 from test_util import check_result
 from sensor_class import sensor
 kx022acr_z = sensor(kx022acr_z)
+import math
 
 def test_sampling_frequency(command):
+    ### In case sampling frequency test is the first test for anything.
+    ### A dummy read is needed before setting watermark!
+
+    dummy_read = kx022acr_z.read_timestamps(command)
+    kx022acr_z.set_watermark(command, 5)
 
     for frequency in kx022acr_z.board.data['settings']['sampling_frequency']['list_values']:
-        result = kx022acr_z.test_sampling_frequency_match_timestamp(frequency, command, tolerance=0.02)
-        result['sampling_rate'] = frequency
+        count = (math.ceil(frequency) * 2)
+        result = kx022acr_z.test_sampling_frequency_match_timestamp(command, frequency, count=count, tolerance=5)
 
         check_result(result)
