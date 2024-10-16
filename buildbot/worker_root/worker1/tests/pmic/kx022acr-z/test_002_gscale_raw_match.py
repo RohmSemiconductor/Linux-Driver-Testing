ce6c8bd4cd67a5fcdb52ae3165e6a9799e388776
Kalle Niemi <kaleposti@gmail.com>
2024-10-16 13:29:43 +0300

    Tests: sensor class: Modify test_gscale_raw()
    sensor_class.py:
    
    test_gscale_raw() function name suggests that it is for testing the raw
    values, but the function is becoming a generic function that can test
    multiple gscale related features.
    
    Rename test_gscale_raw() to test_gscale()
    Modify ['stage'] from 'gscale_raw_'+test_type' to 'gscale_'+test_type
    
    There is a bigger problem with setting high_limit and low_limit when
    the expected value is negative. Also the limit values are moved out of
    the 'if test_type ==' block.
    
    Swap high_limit and low_limit when value is less than 0.
    
    test_002_gscale_raw_match.py:
    
    Update function call to match the modified test_gscale_raw() in
    sensor_class.py
    
    ----
    Additional fix in the sensor_class.py is to replace hardcoded 'z' with
    variable 'axis' which carries the axis string to be tested.

--- buildbot/worker_root/worker1/tests/pmic/kx022acr-z/test_002_gscale_raw_match.py
+++ buildbot/worker_root/worker1/tests/pmic/kx022acr-z/test_002_gscale_raw_match.py
@@ -10,7 +10,8 @@ kx022acr_z = sensor(kx022acr_z)
 ### Only Z-axis is tested in this test. It is the only axis which' value can be relied to be
 ### fairly close to earths gravitational acceleration.
 def test_gscale_raw_match(command):
-    result = kx022acr_z.test_gscale_raw(command, 'z', 'match', tolerance=4)
-    result = kx022acr_z.test_gscale_raw(command, 'y', 'match', tolerance=3, append_results=True)
-    result = kx022acr_z.test_gscale_raw(command, 'x', 'match', tolerance=3, append_results=True)
+    result = kx022acr_z.test_gscale(command, 'z', 'raw_match', tolerance=4)
+    result = kx022acr_z.test_gscale(command, 'y', 'raw_match', tolerance=3, append_results=True)
+    result = kx022acr_z.test_gscale(command, 'x', 'raw_match', tolerance=3, append_results=True)
+
     check_result(result)
