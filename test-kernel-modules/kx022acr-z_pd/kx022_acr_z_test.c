7ed72e506246dc278178f30d764a251a936220e6
Kalle Niemi <kaleposti@gmail.com>
2024-11-07 08:26:02 +0200

    Test module: KX022ACR-Z: Attempt to read buffer
    It is benefical to test reading the buffer. This is a bit messy state of
    code, but some what functional.
    
    Commented out struct scan from the cb function and moved to be global.
    
    Added iio_channel_stop_all_cb() calls to _raw_show functions so that
    the buffer do not have to be explicitly stopped.
    
    _buffer_show() functions work, but they only print one value.
    
    read_buffer_store() is an attempt to read and print multiple values from
    buffer. buffer2list_store/show will be another attempt at this.

--- test-kernel-modules/kx022acr-z_pd/kx022_acr_z_test.c
+++ test-kernel-modules/kx022acr-z_pd/kx022_acr_z_test.c
@@ -7,11 +7,11 @@
 #include <linux/iio/types.h>
 #include <linux/iio/buffer.h>
 #include <linux/container_of.h>
+#include <linux/kfifo.h>
 
 struct iio_channel *g_chan;
 struct iio_cb_buffer *g_buf;
 
-/*
 struct scan {
 	__le16 channels[3];
 	s64 ts __aligned(8);
@@ -21,8 +21,14 @@ struct accel_priv {
 	struct scan *scan;
 };
 
-struct accel_priv *cb_priv;
-*/
+struct accel_priv cb_priv;
+
+struct accel_test {
+	unsigned int watermark;
+};
+
+struct accel_test g_accel_test;
+
 const unsigned int ms2_mult = 1000;
 
 enum INT_TO_FRAC {
@@ -57,6 +63,139 @@ static int char_to_frac(char f[], enum INT_TO_FRAC FRAC)
 	pr_info("res: %d\n", res);
 	return res;
 }
+
+static ssize_t x_buffer_show (struct kobject *ko, struct kobj_attribute *a, char *b)
+{
+	int rval;
+	struct iio_cb_buffer *buff = g_buf;
+
+	rval = iio_channel_start_all_cb(buff);
+	pr_info("iio_channel_start_all_cb called!\n");
+
+	rval = sprintf(b, "%d\n", cb_priv.scan->channels[0]);
+
+	return rval;
+}
+
+static struct kobj_attribute x_buffer = __ATTR_RO(x_buffer);
+
+static ssize_t y_buffer_show (struct kobject *ko, struct kobj_attribute *a, char *b)
+{
+	int rval;
+	struct iio_cb_buffer *buff = g_buf;
+
+	rval = iio_channel_start_all_cb(buff);
+	pr_info("iio_channel_start_all_cb called!\n");
+
+	rval = sprintf(b, "%d\n", cb_priv.scan->channels[1]);
+
+	return rval;
+}
+
+static struct kobj_attribute y_buffer = __ATTR_RO(y_buffer);
+
+static ssize_t z_buffer_show (struct kobject *ko, struct kobj_attribute *a, char *b)
+{
+	int rval;
+	struct iio_cb_buffer *buff = g_buf;
+
+	rval = iio_channel_start_all_cb(buff);
+	pr_info("iio_channel_start_all_cb called!\n");
+
+	if (rval != 0)
+		pr_err("iio_channel_start_all_cb: %d\n", rval);
+
+	rval = sprintf(b, "%d\n", cb_priv.scan->channels[2]);
+
+	return rval;
+}
+
+static struct kobj_attribute z_buffer = __ATTR_RO(z_buffer);
+
+static ssize_t buffer2list_show (struct kobject *ko, struct kobj_attribute *a, char *b)
+{
+	int rval;
+	return rval;
+}
+static ssize_t buffer2list_store (struct kobject *ko, struct kobj_attribute *a, const char *b, size_t c)
+{
+	int rval;
+
+	return rval;
+}
+
+static struct kobj_attribute buffer2list = __ATTR_RW(buffer2list);
+
+static ssize_t read_buffer_show (struct kobject *ko, struct kobj_attribute *a, char *b)
+{
+	int rval;
+
+	rval = sprintf(b, "%d\n", sizeof(*b));
+	rval += sprintf(b + rval, "%d\n", sizeof(*b));
+
+	return rval;
+}
+static ssize_t read_buffer_store (struct kobject *ko, struct kobj_attribute *a, const char *b, size_t c)
+{
+	int rval = -EINVAL;
+	unsigned int watermark;
+	char axis[2];
+	int xyz_match;
+
+	struct iio_cb_buffer *buff = g_buf;
+
+	iio_channel_stop_all_cb(buff);
+	pr_info("iio_channel_stop_all_cb called!\n");
+
+	if (2 == sscanf(b, "%s %d", axis, &watermark))
+	{
+		rval = iio_channel_cb_set_buffer_watermark(buff,watermark);
+		if (rval != 0)
+			pr_err("watermark error: %d\n", rval);
+		else
+			g_accel_test.watermark = watermark;
+
+	}
+	else if (1 == sscanf(b,"%s", axis))
+	{
+		xyz_match = !(!strncmp(axis, "x", 2) ^ !strncmp(axis, "y", 2) ^ !strncmp(axis, "z", 2));
+		pr_info("xyz_match: %d\n", xyz_match);
+
+		if (xyz_match == 0)
+		{
+			pr_info("if or or sisalla\n");
+			watermark = g_accel_test.watermark;
+			rval = iio_channel_cb_set_buffer_watermark(buff,watermark);
+		}
+		else
+			pr_info("%d",strncmp(axis, "x",2));
+	}
+	else
+		pr_err("sscanf return: %d\n", rval);
+
+	if (!rval)
+	{
+		pr_info("okei, nyt ollaan !rvalissa\n");
+		rval = iio_channel_start_all_cb(buff);
+		pr_info("iio_channel_start_all_cb called!\n");
+
+		for (int x = 0; x < g_accel_test.watermark; x++)
+		{
+			pr_info("for loopissa: %d\n",x);
+			if (strncmp(axis, "x",1) == 0)
+			{
+				/* This does not get new values */
+				pr_info("%d\n", cb_priv.scan->channels[0]);
+			}
+		}
+	}
+
+	rval = c;
+	return rval;
+}
+
+static struct kobj_attribute read_buffer = __ATTR_RW(read_buffer);
+
 
 static ssize_t scale_available_show (struct kobject *ko, struct kobj_attribute *a, char *b)
 {
@@ -124,8 +263,8 @@ static ssize_t hwfifo_watermark_show (struct kobject *ko, struct kobj_attribute 
 {
 	int rval;
 
-	rval = sprintf(b, "Write only :D\n");
-	pr_err("write only :D %d\n", rval);
+	rval = sprintf(b, "Write only\n");
+	pr_info("Write only\n");
 
 
 	return rval;
@@ -155,9 +294,8 @@ static ssize_t watermark_show (struct kobject *ko, struct kobj_attribute *a, cha
 {
 	int rval;
 
-	rval = sprintf(b, "Write only :D\n");
-	pr_err("write only :D %d\n", rval);
-
+	rval = sprintf(b, "%d\n", g_accel_test.watermark);
+	pr_info("write only\n");
 
 	return rval;
 }
@@ -180,6 +318,9 @@ static ssize_t watermark_store (struct kobject *ko, struct kobj_attribute *a, co
 
 	if (rval != 0)
 		pr_err("watermark error: %d\n", rval);
+	else
+		g_accel_test.watermark = watermark;
+
 	rval = iio_channel_start_all_cb(buff);
 	pr_info("iio_channel_start_all_cb called!\n");
 
@@ -249,7 +390,7 @@ static ssize_t sampling_frequency_store (struct kobject *ko, struct kobj_attribu
 
 static struct kobj_attribute sampling_frequency = __ATTR_RW(sampling_frequency);
 
-static ssize_t in_scale_show (struct kobject *ko, struct kobj_attribute *a, char *b)
+static ssize_t scale_show (struct kobject *ko, struct kobj_attribute *a, char *b)
 {
 	int rval;
 	int val;
@@ -259,13 +400,12 @@ static ssize_t in_scale_show (struct kobject *ko, struct kobj_attribute *a, char
 	rval = iio_read_channel_attribute(chan, &val, &val2, IIO_CHAN_INFO_SCALE);
 	if (rval < 0)
 		pr_err("SCALE: iio_read_channel_attribute errno %d\n", rval);
-
 	rval = sprintf(b, "%d.%09d\n", val, val2);
 
 	return rval;
 }
 
-static ssize_t in_scale_store (struct kobject *ko, struct kobj_attribute *a, const char *b, size_t c)
+static ssize_t scale_store (struct kobject *ko, struct kobj_attribute *a, const char *b, size_t c)
 {
 	int rval;
 	int val;
@@ -295,18 +435,22 @@ static ssize_t in_scale_store (struct kobject *ko, struct kobj_attribute *a, con
 	return rval;
 }
 
-static struct kobj_attribute in_scale = __ATTR_RW(in_scale);
+static struct kobj_attribute scale = __ATTR_RW(scale);
 
 static ssize_t x_raw_show (struct kobject *ko, struct kobj_attribute *a, char *b)
 {
 	int rval;
 	int val;
 	struct iio_channel *chan = g_chan;
+	struct iio_cb_buffer *buff = g_buf;
+
+	iio_channel_stop_all_cb(buff);
+	pr_info("iio_channel_stop_all_cb called!\n");
 
 	rval = iio_read_channel_raw(&chan[0], &val );
 
 	if (rval < 0)
-		pr_err("iio_read_channel_raw errno: %d\n", val);
+		pr_err("iio_read_channel_raw errno: %d\n", rval);
 
 	rval = sprintf(b,"%d\n",val);
 
@@ -319,12 +463,16 @@ static ssize_t y_raw_show (struct kobject *ko, struct kobj_attribute *a, char *b
 {
 	int rval;
 	struct iio_channel *chan = g_chan;
+	struct iio_cb_buffer *buff = g_buf;
 	int val;
+
+	iio_channel_stop_all_cb(buff);
+	pr_info("iio_channel_stop_all_cb called!\n");
 
 	rval = iio_read_channel_raw(&chan[1], &val );
 
 	if (rval < 0)
-		pr_err("iio_read_channel_raw errno: %d\n", val);
+		pr_err("iio_read_channel_raw errno: %d\n", rval);
 
 	rval = sprintf(b,"%d\n",val);
 
@@ -337,12 +485,16 @@ static ssize_t z_raw_show (struct kobject *ko, struct kobj_attribute *a, char *b
 {
 	int rval;
 	struct iio_channel *chan = g_chan;
+	struct iio_cb_buffer *buff = g_buf;
 	int val;
+
+	iio_channel_stop_all_cb(buff);
+	pr_info("iio_channel_stop_all_cb called!\n");
 
 	rval = iio_read_channel_raw(&chan[2], &val );
 
 	if (rval < 0)
-		pr_err("iio_read_channel_raw errno: %d\n", val);
+		pr_err("iio_read_channel_raw errno: %d\n", rval);
 
 	rval = sprintf(b,"%d\n",val);
 
@@ -417,39 +569,41 @@ static ssize_t z_ms2_show (struct kobject *ko, struct kobj_attribute *a, char *b
 
 static struct kobj_attribute z_ms2 = __ATTR_RO(z_ms2);
 
-/*
 static ssize_t timestamp_show (struct kobject *ko, struct kobj_attribute *a, char *b)
 {
 	int rval;
-	struct iio_channel *chan = g_chan;
-	int val;
+//	struct iio_channel *chan = g_chan;
 
+	/*
 	rval = iio_read_channel_raw(&chan[3], &val);
 
 	if (rval < 0)
-		pr_err("iio_read_channel_raw errno: %d\n", val);
-
-	rval = sprintf(b,"%d\n",val);
+		pr_err("iio_read_channel_raw errno: %d\n", cb_priv.scan->ts);
+	*/
+	rval = sprintf(b,"%lld\n",cb_priv.scan->ts);
 
 	return rval;
 }
 static struct kobj_attribute timestamp = __ATTR_RO(timestamp);
-*/
 
 static struct attribute *test_kx022acr_z_chattrs[] = {
+	&read_buffer.attr,
+	&x_buffer.attr,
+	&y_buffer.attr,
+	&z_buffer.attr,
 	&scale_available.attr,
 	&sampling_frequency_available.attr,
 	&hwfifo_watermark.attr,
 	&watermark.attr,
 	&sampling_frequency.attr,
-	&in_scale.attr,
+	&scale.attr,
 	&x_raw.attr,
 	&y_raw.attr,
 	&z_raw.attr,
 	&x_ms2.attr,
 	&y_ms2.attr,
 	&z_ms2.attr,
-//	&timestamp.attr,
+	&timestamp.attr,
 	NULL
 };
 
@@ -467,34 +621,32 @@ static const struct attribute_group *test_kx022acr_z_attr_groups[] = {
 
 static int cb(const void *data, void *private)
 {
-//	struct accel_priv *cb_priv = private;
-
+/*
 	struct scan {
 		__le16 channels[3];
 		s64 ts __aligned(8);
 	};
-
+*/
 	struct scan *buf_data;
 
 	buf_data = (struct scan*)data;
 
-//	cb_priv->scan = (struct scan*)data;
+	cb_priv.scan = (struct scan*)data;
 
+/*
 	pr_info("CB FUNC CALLED!");
 
 	pr_info("Chan0: %d\n", buf_data->channels[0]);
 	pr_info("Chan1: %d\n", buf_data->channels[1]);
 	pr_info("Chan2: %d\n", buf_data->channels[2]);
-
+*/
 	return 0;
 }
-
-
 
 static int test_kx022acr_z_probe(struct platform_device *pdev)
 {
 	int retval = 0;
-	dev_info(&pdev->dev, "Ver 015\n");
+	dev_info(&pdev->dev, "Ver 021\n");
 	int vcb = 0;
 	void *vcbp = &vcb;
 
@@ -513,6 +665,9 @@ static int test_kx022acr_z_probe(struct platform_device *pdev)
 		retval = PTR_ERR(g_buf);
 		dev_info(&pdev->dev, "Error code: %d ", retval);
 	}
+
+
+	retval = iio_channel_start_all_cb(g_buf);
 	return retval;
 }
 
