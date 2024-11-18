e29ddabfd39efc3f79e52f2b9815b1b5116eab12
Kalle Niemi <kaleposti@gmail.com>
2024-11-18 14:18:46 +0200

    Test modules: Accel integrity test: Add mutex
    This is still a test version, the code has commented out thread creation
    for a sync thread that was tried.
    
    Debug time prints show around 400 microseconds difference between
    threads.

--- test-kernel-modules/accel_integrity_test/accel_integrity_test.c
+++ test-kernel-modules/accel_integrity_test/accel_integrity_test.c
@@ -3,11 +3,28 @@
 #include <string.h>
 #include <stdio.h>
 #include <getopt.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/time.h>
 
 struct arguments  {
 	char *samplerate;
 	char *scale;
 };
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+
+
+void* trigger_thread(void* vargs)
+{
+	pthread_mutex_lock(&mutex);
+	pthread_cond_wait(&cond, &mutex);
+	pthread_mutex_unlock(&mutex);
+	printf("trigger_thread: released mutex\n");
+	return NULL;
+}
+
 
 void* thread_samplerate(void* vargs)
 {
@@ -16,17 +33,27 @@ void* thread_samplerate(void* vargs)
 	FILE *p;
 	struct arguments *arguments;
 
+	struct timeval tt;
 	arguments = (struct arguments*)vargs;
 
+
 	printf("thread_samplerate: %s\n", arguments->samplerate);
+	printf("thread_samplerate: waiting for release\n");
 
 	strcat(command, "echo ");
 	strcat(command, arguments->samplerate);
 	strcat(command, " > /sys/devices/platform/generic_accel_test/iio_channels/sampling_frequency");
 
-	printf("samplerate command: %s\n", command);
+
+	pthread_mutex_lock(&mutex);
+	pthread_cond_wait(&cond, &mutex);
+	pthread_mutex_unlock(&mutex);
+
+	gettimeofday(&tt, NULL);
+	printf("Started at: %ld\n", tt.tv_usec);
 
 	p = popen(command, "r");
+	printf("samplerate command: %s\n", command);
 
 	if (p == NULL)
 		printf("Unable to run: sample rate command\n");
@@ -43,15 +70,26 @@ void* thread_scale(void *vargs)
 	int ch;
 	struct arguments *arguments;
 
+	struct timeval tt;
 	arguments = (struct arguments*)vargs;
 
+
 	printf("thread scale: %s\n", arguments->scale);
+	printf("thread_scale: waiting for release\n");
 
 	strcat(command, "echo ");
 	strcat(command, arguments->scale);
 	strcat(command, " > /sys/devices/platform/generic_accel_test/iio_channels/scale");
 
+	pthread_mutex_lock(&mutex);
+	pthread_cond_wait(&cond, &mutex);
+	pthread_mutex_unlock(&mutex);
+
+	gettimeofday(&tt, NULL);
+	printf("Started at: %ld\n", tt.tv_usec);
+
 	p = popen(command, "r");
+	printf("scale command: %s\n", command);
 
 	if (p == NULL)
 		printf("Unable to run: scale command\n");
@@ -66,6 +104,7 @@ int main(int argc, char* argv[])
 	int c;
 	pthread_t thread_id1;
 	pthread_t thread_id2;
+	pthread_t thread_id3;
 	struct arguments *arguments;
 
 	arguments = malloc(sizeof(struct arguments));
@@ -81,14 +120,17 @@ int main(int argc, char* argv[])
 		}
 	}
 
-	printf("Before Thread\n");
+	printf("Before threads\n");
 
-	pthread_create(&thread_id1, NULL, thread_samplerate, (void*)arguments);
-	pthread_create(&thread_id2, NULL, thread_scale, (void*)arguments);
-
-	pthread_join(thread_id1, NULL);
+//	pthread_create(&thread_id1, NULL, trigger_thread, NULL);
+	pthread_create(&thread_id2, NULL, thread_samplerate, (void*)arguments);
+	pthread_create(&thread_id3, NULL, thread_scale, (void*)arguments);
+	sleep(2);
+	pthread_cond_broadcast(&cond);
+//	pthread_join(thread_id1, NULL);
 	pthread_join(thread_id2, NULL);
-	printf("After Thread\n");
+	pthread_join(thread_id3, NULL);
+	printf("After threads\n");
 
 	return 0;
 }
