a2d285dd1a8506e011c633d01c50f32f6c50243e
Kalle Niemi <kaleposti@gmail.com>
2025-04-15 15:29:14 +0300

    Tests: BD79703 conf: Change DAC and add a channel
    BD79703 tests are being tested with BD79124 instead of the BeagleBones
    ADC. Change config file accordingly.

--- buildbot/master_root/master/workers/Test_Worker/tests/driver_tests/configs/bd79703.py
+++ buildbot/master_root/master/workers/Test_Worker/tests/driver_tests/configs/bd79703.py
@@ -1,22 +1,23 @@
 data={
 'name':'bd79703',
-'i2c':{
-    'bus':      2,
-    'address':  0x1E,
-    },
+'adc': 'BeagleBone Black DAC',
+'driver': 'rohm-bd79703.c',
 
 'iio_device':{
     'name':     'bd79703',
-    'adc':      'TI-am335x-adc.2.auto',
+#    'adc':      'TI-am335x-adc.2.auto',
+    'adc':     'bd79124',
     },
 
 'info':{
     'bits':8,
-    'volt_scale':1.8,
-#    'channels':[0,1,2,3,4,5],
+
+    ### Channels are mapped in this manner
+    ### DAC channel : ADC channel
     'channels':{
         0:0,
         1:1,
+        2:2,
     }
     }
 }
