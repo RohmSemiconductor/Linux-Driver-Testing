23d835256703747c2222f99906f038d87c59b3ef
Kalle Niemi <kaleposti@gmail.com>
2025-04-17 13:29:30 +0300

    Tests: BD79703/BD78124: Read stable voltage
    The tolerance is not quite enough to pass the test.
    Also 'assert 1 == 0' is used to forcefully fail the test.
    
    Fix by increasing the tolerance to 10 mV and remove the assert line.

--- buildbot/master_root/master/workers/Test_Worker/tests/driver_tests/bd79703/test_000_read_stable_voltage_bd79124.py
+++ buildbot/master_root/master/workers/Test_Worker/tests/driver_tests/bd79703/test_000_read_stable_voltage_bd79124.py
@@ -8,7 +8,6 @@ from addac_class import addac
 bd79124 = addac(bd79124)
 
 def test_read_stable_voltage(command):
-    result = bd79124.check_stable_voltage(command, bd79124.board.data['info']['stable_voltage_channel'], 2)
+    result = bd79124.check_stable_voltage(command, bd79124.board.data['info']['stable_voltage_channel'], 10)
 
     check_result(result)
-    assert 1 == 0
