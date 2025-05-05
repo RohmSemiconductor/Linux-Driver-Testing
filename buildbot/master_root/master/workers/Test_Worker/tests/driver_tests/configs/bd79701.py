1e3eeba6a5b400babf56f339dfbd597fd4854667
Kalle Niemi <kaleposti@gmail.com>
2025-05-05 12:31:45 +0300

    Tests: Add support for BD79701/BD79104
    BD79701 DAC is tested with BD79104 ADC (atleast for now).
    
    The tests are the same as for BD79703 DAC, except the write_read test
    uses higher tolerance.

--- buildbot/master_root/master/workers/Test_Worker/tests/driver_tests/configs/bd79701.py
+++ buildbot/master_root/master/workers/Test_Worker/tests/driver_tests/configs/bd79701.py
@@ -1,17 +1,29 @@
 data={
-'name':'bd79701',
-'i2c':{
-    'bus':      2,
-    'address':  0x1E,
-    },
+'name':     'bd79701',
+'type':     'DAC',
+'adc':      'BeagleBone Black DAC',
+'driver':   'rohm-bd79703.c',
 
 'iio_device':{
     'name':     'bd79701',
+#    'adc':      'TI-am335x-adc.2.auto',
+    'adc':     'bd79104',
     },
 
 'info':{
     'bits':8,
-    'volt_scale':3.3,
-    'channels':[0,1,2],
+
+    ### Channels are mapped in this manner
+    ### DAC channel : ADC channel
+    ###
+    ### This Dictionary is used to write,
+    ### and then read corresponding ADC
+    ### channel
+
+    'channels':{
+        0:2,
+        1:3,
+        2:4,
+        }
     }
 }
