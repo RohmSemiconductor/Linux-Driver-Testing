7a52981cbdd3e8fb4faad1d121df2d23b678208a
KalleNiemi <kaleposti@gmail.com>
2025-07-22 10:50:21 +0300

    Tests: BD79703: Cleanup write_read test
    Remove left overs from developing the tests.

--- buildbot/master_root/master/workers/Test_Worker/tests/driver_tests/bd79703/test_002_write_read.py
+++ buildbot/master_root/master/workers/Test_Worker/tests/driver_tests/bd79703/test_002_write_read.py
@@ -10,10 +10,8 @@ bd79703 = addac(bd79703)
 def test_write_read(command):
     sysfs_retval = bd79703.get_sysfs_information(command)
     maxval = bd79703.bits_maxval()
-    print(sysfs_retval)
     for channel in bd79703.board.data['info']['channels'].keys():
         for x in range(0, maxval+1):
-#        for x in range(0, 1):
             result = bd79703.write_and_read_value(command, channel, x, tolerance=150)
             check_result(result)
         set_output_zero = bd79703.write_and_read_value(command, channel, 0)
