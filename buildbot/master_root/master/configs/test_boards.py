8f7795f9e2b7a7221c1dac21001128b4b1e35a81
KalleNiemi <kaleposti@gmail.com>
2026-01-05 08:50:07 +0200

    Test boards update: Remove commented out code
    Commented out code is old style. Should be removed.
    
    Remove commented out code.

--- buildbot/master_root/master/configs/test_boards.py
+++ buildbot/master_root/master/configs/test_boards.py
@@ -1,32 +1,5 @@
 test_boards ={}
 
-#test_boards['beagle1']={
-#'name':'beagle1',
-#'products':['bd9576','bd71847'],
-#'power_port':'1',
-#'arch':'arm32',
-#}
-#
-#test_boards['beagle2']={
-#'name':'beagle2',
-#'products':['bd71815'],
-#'power_port':'2',
-#'arch':'arm32',
-#}
-#
-#test_boards['beagle3']={
-#'name':'beagle3',
-#'products':['bd71828'],
-#'power_port':'3',
-#'arch':'arm32',
-#}
-#
-#test_boards['beagle4']={
-#'name':'beagle4',
-#'products':['bd96801', 'bd71837'],
-#'power_port':'4',
-#'arch':'arm32',
-#}
 test_boards['pmic'] = {
     'power_ports':{
         '0':{
